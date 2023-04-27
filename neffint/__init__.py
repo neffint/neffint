6731f83aaaefd6d405b4f6c7bcaa80bf6802bc70
Eskil Vik <eskil.vik@cern.ch>
2023-04-27 17:44:33 +0200

    rewrite __init__.py to not rely on strings

--- neffint/__init__.py
+++ neffint/__init__.py
@@ -1,6 +1,2 @@
-from .fixed_grid_fourier_integral import *
-
-# Define which functions, classes etc. are part of the public interface
-__all__ = [
-    "fourier_integral_fixed_sampling"
-]
+# Bring into this namespace everything that should be available when importing neffint
+from .fixed_grid_fourier_integral import fourier_integral_fixed_sampling
