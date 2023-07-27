1c14c105380ecf8bdb6ccd5b337f9b3a967150e0
Eskil Vik <80857458+veskil@users.noreply.github.com>
2023-07-27 18:32:44 +0200

    Fix bug in `__init__.py`

--- neffint/__init__.py
+++ neffint/__init__.py
@@ -6,7 +6,8 @@ from .fixed_grid_fourier_integral import fourier_integral_fixed_sampling
 __all__= [
     "fourier_integral_fixed_sampling",
     "fourier_integral_adaptive",
-    "improve_frequency_range"
+    "improve_frequency_range",
+]
 
 from ._version import __version__
 
