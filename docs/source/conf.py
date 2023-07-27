cca2ba424808a562bf302e3da185892690470044
Eskil Vik <eskil.vik@cern.ch>
2023-07-27 17:13:34 +0200

    add docs/requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
